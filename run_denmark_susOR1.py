73e041f0a5c38447094a10afef862e2e2314fbab
Robyn Stuart <robyna.s@gmail.com>
2021-03-15 10:25:15 +0100

    add latest calibration

--- run_denmark_susOR1.py
+++ run_denmark_susOR1.py
@@ -18,6 +18,7 @@ cv.git_info('covasim_version.json')
 # Saving and plotting settings
 do_plot = 1
 do_save = 1
+keep_people=0
 save_sim = 1
 do_show = 0
 verbose = 1
@@ -30,8 +31,6 @@ to_plot = sc.objdict({
     'New infections': ['new_infections'],
     'New tests': ['new_tests'],
     'Cumulative deaths': ['cum_deaths'],
-    'R': ['r_eff'],
-    'Cumulative hospitalisations': ['cum_severe'],
 })
 
 
@@ -39,6 +38,7 @@ to_plot = sc.objdict({
 runoptions = ['quickfit', # Does a quick preliminary calibration. Quick to run, ~30s
               'fullfit',  # Searches over parameters and seeds (10,000 runs) and calculates the mismatch for each. Slow to run: ~1hr
               'finialisefit', # Filters the 10,000 runs from the previous step, selects the best-fitting ones, and runs these
+              'quickscens',
               'scens', # Takes the best-fitting runs and projects these forward under different mask and TTI assumptions
               'tti_sweeps', # Sweeps over future testing/tracing values to create data for heatmaps
               ]
@@ -58,7 +58,7 @@ data_end = '2021-01-17' # Final date for calibration
 # Create the baseline simulation
 ########################################################################
 
-def test_num_subtarg(sim, sev=0.9, u20=0.02):
+def test_num_subtarg(sim, sev=100.0, u20=0.5):
     ''' Subtarget severe people with more testing, and young people with less '''
     sev_inds = sim.people.true('severe')
     u20_inds = sc.findinds(sim.people.age<20 * ~sim.people.severe) # People who are under 20 and severe test as if they're severe; * is element-wise "and"
@@ -68,6 +68,10 @@ def test_num_subtarg(sim, sev=0.9, u20=0.02):
     vals = np.concatenate([u20_vals, sev_vals])
     return {'inds':inds, 'vals':vals}
 
+def vaccinate_8085(sim):
+    return dict(inds=cv.true(sim.people.age > 80), vals=0.9)
+def vaccinate_6580(sim):
+    return dict(inds=cv.true((sim.people.age>65)*(sim.people.age<80)), vals=0.9)
 
 def make_sim(seed, p, calibration=True, scenname=None, end_day=None, verbose=0):
 
@@ -98,47 +102,50 @@ def make_sim(seed, p, calibration=True, scenname=None, end_day=None, verbose=0):
 
     sim = cv.Sim(pars=pars, datafile=data_path, location='denmark')
     sim['prognoses']['symp_probs'] = np.array([0.2, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.9])
+    sim['prognoses']['sus_ORs'][0] = 1.0 # ages 0-10
+    sim['prognoses']['sus_ORs'][1] = 1.0 # ages 10-20
 
     # 1. Lockdowns and NPIs
     interventions = [
-        cv.clip_edges(days=['2020-03-13', '2020-04-15', '2020-06-15', '2020-09-01', '2020-12-09', '2020-12-19'], changes=[0.05, 0.8, 0.1, 1.0, 0.5, 0.0], layers=['s']), # School closure and reopening
+        cv.clip_edges(days=['2020-03-13', '2020-04-15', '2020-06-15', '2020-09-01', '2020-12-09', '2020-12-19'], changes=[0.05, 0.8, 0.1, 1.0, 5/13, 0.02], layers=['s']), # School closure and reopening
         cv.change_beta(['2020-03-13', '2020-09-01', '2020-10-29'], [0.6, 0.8, 0.67], layers=['s']), # Assume precautions in place after school returns
 
         cv.clip_edges(days=['2020-03-13', '2020-04-15', '2020-06-15', '2020-09-01', '2020-12-09', '2020-12-19'], changes=[0.1, 0.3, 0.5, 0.8, 0.5, 0.1], layers=['w']),
         cv.change_beta(['2020-03-13', '2020-09-01', '2020-10-29'], [0.6, 0.8, 0.67], layers=['w']),  # Assume precautions in place for workers
 
         cv.change_beta(['2020-03-13', '2020-04-15', '2020-12-09', '2020-12-19', '2021-01-03'], [1.2, 1.0, 1.2, 1.5, 1.2], layers=['h']),
-        cv.change_beta(['2020-03-13', '2020-04-15', '2020-06-15', '2020-09-01', '2020-10-29', '2020-12-09', '2020-12-19'], [0.3, 0.5, 0.65, 0.8, 0.67, 0.6, 0.5], layers=['c']),
+        cv.change_beta(['2020-03-13', '2020-04-15', '2020-06-15', '2020-09-01', '2020-10-29', '2020-12-09', '2020-12-19', '2021-01-03'], [0.3, 0.5, 0.65, 0.8, 0.67, 0.6, 0.5, 0.3], layers=['c']),
     ]
 
     if not calibration:
-        if scenname == 'lift17':
+        if scenname == 'lift07':
             interventions += [
-                cv.clip_edges( days=['2021-01-18'], changes=[1.0], layers=['s']),
-                cv.clip_edges( days=['2021-01-18'], changes=[0.8], layers=['w']),
-                cv.change_beta(days=['2021-01-18'], changes=[0.67], layers=['c'])]
-        elif scenname == 'lift31':
+                cv.clip_edges( days=['2021-02-08'], changes=[1.0], layers=['s']),
+                cv.clip_edges( days=['2021-02-08'], changes=[0.5], layers=['w']),
+                cv.change_beta(days=['2021-02-08'], changes=[0.67], layers=['c'])]
+        elif scenname == 'lift21':
             interventions += [
-                cv.clip_edges( days=['2021-02-01'], changes=[1.0], layers=['s']),
-                cv.clip_edges( days=['2021-02-01'], changes=[0.8], layers=['w']),
-                cv.change_beta(days=['2021-02-01'], changes=[0.67], layers=['c'])]
-        elif scenname == 'schools17rest30':
+                cv.clip_edges( days=['2021-02-22'], changes=[1.0], layers=['s']),
+                cv.clip_edges( days=['2021-02-22'], changes=[0.5], layers=['w']),
+                cv.change_beta(days=['2021-02-22'], changes=[0.67], layers=['c'])]
+        elif scenname == 'phased':
             interventions += [
-                cv.clip_edges( days=['2021-01-18'], changes=[1.0], layers=['s']),
-                cv.clip_edges( days=['2021-02-01'], changes=[0.8], layers=['w']),
-                cv.change_beta(days=['2021-02-01'], changes=[0.67], layers=['c'])]
+                cv.clip_edges( days=['2021-02-22', '2021-03-08'], changes=[5/13, 1.0], layers=['s']),
+                cv.clip_edges( days=['2021-03-08', '2021-03-22'], changes=[0.5, 0.8], layers=['w']),
+                cv.change_beta(days=['2021-03-08', '2021-03-22'], changes=[0.5, 0.67], layers=['c'])]
 
     # 2. Testing assumptions
     interventions += [
-#        cv.test_num(daily_tests=sim.data['new_tests'], start_day=0, end_day=sim.day(data_end), test_delay=1,
-#                    symp_test=p.tn, sensitivity=0.97, subtarget=test_num_subtarg),
-#        cv.test_prob(symp_prob=0.15, start_day=sim.day(data_end)+1, test_delay=1)
+        cv.test_num(daily_tests=sim.data['new_tests'], start_day=0, end_day=sim.day(data_end), test_delay=1,
+                    symp_test=p.tn, sensitivity=0.97, subtarget=test_num_subtarg),
+        cv.test_prob(symp_prob=0.15, subtarget=test_num_subtarg, start_day=sim.day(data_end)+1, test_delay=1)
 #        cv.test_num(daily_tests=100_000, start_day=sim.day(data_end)+1, test_delay=1,
 #                    symp_test=p.tn, sensitivity=0.97, subtarget=test_num_subtarg)
-        cv.test_prob(symp_prob=0.050, asymp_quar_prob=0.1, subtarget=test_num_subtarg, start_day=0, end_day='2020-04-30', test_delay=2),
-        cv.test_prob(symp_prob=0.100, asymp_quar_prob=0.15, subtarget=test_num_subtarg, start_day='2020-05-01', end_day='2020-08-31', test_delay=1),
-        cv.test_prob(symp_prob=0.125, asymp_quar_prob=0.175, subtarget=test_num_subtarg, start_day='2020-09-01', test_delay=1),
-        cv.test_prob(symp_prob=0.150, asymp_quar_prob=0.2, subtarget=test_num_subtarg, start_day=sim.day(data_end) + 1, test_delay=1)
+
+#        cv.test_prob(symp_prob=0.1, asymp_quar_prob=0.1, start_day=0, end_day='2020-04-30', test_delay=2),
+#        cv.test_prob(symp_prob=0.2, asymp_quar_prob=0.2, start_day='2020-05-01', end_day='2020-08-31', test_delay=1),
+#        cv.test_prob(symp_prob=0.3, asymp_quar_prob=0.3, start_day='2020-09-01', test_delay=1),
+#        cv.test_prob(symp_prob=0.15, start_day=sim.day(data_end) + 1, test_delay=1)
     ]
 
     # 3. Assume some amount of contact tracing
@@ -153,6 +160,11 @@ def make_sim(seed, p, calibration=True, scenname=None, end_day=None, verbose=0):
 #                                       'rel_severe_prob': {'days': [sim.day('2020-06-01')], 'vals': [0.5]}
 #                                      'n_imports': {'days': [sim.day('2020-06-01'), sim.day('2020-12-09')], 'vals': [2, 0]},
                                         })]
+
+    # Vaccination
+#    vaccine1 = cv.vaccine(days=sim.day('2021-03-31'), rel_sus=0.4, rel_symp=0.2, subtarget=vaccinate_8085)
+#    vaccine2 = cv.vaccine(days=sim.day('2021-04-30'), rel_sus=0.4, rel_symp=0.2, subtarget=vaccinate_6580)
+#    interventions += [vaccine1,vaccine2]
 
     # 5. Add the new variant
     # Add a new change in beta to represent the takeover of the novel variant VOC 202012/01
@@ -179,10 +191,10 @@ def make_sim(seed, p, calibration=True, scenname=None, end_day=None, verbose=0):
 if __name__ == '__main__':
 
     p = sc.objdict(
-        beta = 0.016, # 0.015
-        delta_beta = 0.6, # 0.6
-        rd = 0.6, # 0.45
-        tn = 50.) #50
+        beta = 0.015,
+        delta_beta = 0.6,
+        rd = 0.45,
+        tn = 50.)
 
     betas = [i / 10000 for i in range(146, 156, 2)]
     rds = [i / 100 for i in range(41, 51, 2)]
@@ -198,9 +210,8 @@ if __name__ == '__main__':
             sim.set_seed()
             sim.label = f"Sim {seed}"
             sims.append(sim)
-#        sim.run()
         msim = cv.MultiSim(sims)
-        msim.run(keep_people=True)
+        msim.run()
         msim.reduce()
         if do_plot:
             msim.plot(to_plot=to_plot, do_save=True, do_show=False, fig_path=f'denmark.png',
@@ -268,11 +279,45 @@ if __name__ == '__main__':
                       legend_args={'loc': 'upper left'}, axis_args={'hspace': 0.4}, interval=50, n_cols=2)
 
 
+    elif whattorun=='quickscens':
+
+        # Define scenario to run
+        scenarios = ['lift07', 'lift21', 'phased']
+
+        for scenname in scenarios:
+
+            print('---------------\n')
+            print(f'Beginning scenario: {scenname}')
+            print('---------------\n')
+            sc.blank()
+            p = sc.objdict(beta=0.015,delta_beta=0.6,rd=0.45,tn=50.)
+            s0 = make_sim(seed=1, p=p, calibration=False, scenname=scenname, end_day='2021-05-01', verbose=0.1)
+            sims = []
+            for seed in range(6):
+                sim = s0.copy()
+                sim['rand_seed'] = seed
+                sim.set_seed()
+                sim.label = f"Sim {seed}"
+                sims.append(sim)
+            msim = cv.MultiSim(sims)
+            msim.run(verbose=0.1, keep_people=keep_people)
+
+            if save_sim:
+                msim.save(f'{resfolder}/denmark_scen_{scenname}.obj', keep_people=keep_people)
+            if do_plot:
+                msim.reduce(quantiles=[0.10, 0.90])
+                msim.plot(to_plot=to_plot, do_save=do_save, do_show=False, fig_path=f'denmark_{scenname}_current.png',
+                          legend_args={'loc': 'upper left'}, axis_args={'hspace': 0.4}, interval=50, n_cols=2)
+
+            print(f'... completed scenario: {scenname}')
+
+
+
     # Run scenarios with best-fitting seeds and parameters
     elif whattorun=='scens':
 
         # Define scenario to run
-        scenarios = ['lift17', 'lift30', 'schools17rest30']
+        scenarios = ['lift07', 'lift21', 'phased']
 
         for scenname in scenarios:
 
@@ -310,52 +355,3 @@ if __name__ == '__main__':
             print(f'... completed scenario: {scenname}')
 
 
-    # Run scenarios with best-fitting seeds and parameters
-    elif whattorun=='tti_sweeps':
-
-        symp_test_vals = np.linspace(0, 1, 21)
-        trace_eff_vals = np.linspace(0, 1, 21)
-        scenarios = ['masks30_notschools','masks15','masks30','masks15_notschools']
-
-        # Define scenario to run
-        for scenname in scenarios:
-            sweep_summary = {'cum_inf':[],'peak_inf':[],'cum_death':[]}
-            for future_symp_test in symp_test_vals:
-                daily_test = np.round(1 - (1 - future_symp_test) ** (1 / 10), 3) if future_symp_test<1 else 0.4
-                cum_inf, peak_inf, cum_death = [], [], []
-                for future_t_eff in trace_eff_vals:
-
-                    sc.blank()
-                    print('---------------')
-                    print(f'Scenario: {scenname}, testing: {future_symp_test}, tracing: {future_t_eff}')
-                    print('--------------- ')
-                    sims = []
-                    fitsummary = sc.loadobj(f'{resfolder}/fitsummary.obj')
-
-                    for bn, beta in enumerate(betas):
-                        goodseeds = [i for i in range(n_runs) if fitsummary[bn][i] < 125.5] # Take the best 10
-                        if len(goodseeds) > 0:
-                            s0 = make_sim(1, beta, calibration=False, scenario=scenname, future_symp_test=daily_test, future_t_eff=future_t_eff, end_day='2021-12-31')
-                            for seed in goodseeds:
-                                sim = s0.copy()
-                                sim['rand_seed'] = seed
-                                sim.set_seed()
-                                sim.label = f"Sim {seed}"
-                                sims.append(sim)
-
-                    msim = cv.MultiSim(sims)
-                    msim.run(verbose=-1)
-                    msim.reduce()
-
-                    # Store results
-                    data_end_day = msim.sims[0].day(data_end)
-
-                    cum_inf.append(msim.results['cum_infections'].values[-1])
-                    peak_inf.append(max(msim.results['new_infections'].values[data_end_day:]))
-                    cum_death.append(msim.results['cum_deaths'].values[-1])
-
-                sweep_summary['cum_inf'].append(cum_inf)
-                sweep_summary['peak_inf'].append(peak_inf)
-                sweep_summary['cum_death'].append(cum_death)
-
-            sc.saveobj(f'{resfolder}/denmark_tti_sweeps_{scenname}.obj', sweep_summary)
